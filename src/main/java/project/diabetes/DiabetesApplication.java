c61263f5b106bcce2977d64c6062fcde99910517
USER <cji513@naver.com>
2023-07-25 20:08:34 +0900

    test

--- src/main/java/project/diabetes/DiabetesApplication.java
+++ src/main/java/project/diabetes/DiabetesApplication.java
@@ -8,7 +8,6 @@ public class DiabetesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiabetesApplication.class, args);
-		//test
 	}
 
 }
--- src/main/java/project/diabetes/controller/MainController.java
+++ src/main/java/project/diabetes/controller/MainController.java
@@ -6,36 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/main1") //임시1
+    @GetMapping("/main") //임시1
     public String main1(){
-        return "main1";
+        return "main";
     }
-
-    @GetMapping("/main2") //임시2
-    public String main2(){
-        return "index";
-    }
-
-    @GetMapping("/main2/services")
-    public String services(){
-        return "services";
-    }
-
-    @GetMapping("/main2/about")
-    public String about(){
-        return "about";
-    }
-
-    @GetMapping("/main2/shop")
-    public String shop(){
-        return "shop";
-    }
-
-    @GetMapping("/main2/contact")
-    public String contact(){
-        return "contact";
-    }
-
-
-    // https://www.free-css.com/free-css-templates/page284/built-better
 }
