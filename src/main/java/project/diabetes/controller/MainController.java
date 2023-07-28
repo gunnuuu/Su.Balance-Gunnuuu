1be39ecad438c1a5be63aafb41f648a1c8d6f0cf
USER <cji513@naver.com>
2023-07-28 18:40:39 +0900

    commit

--- src/main/java/project/diabetes/controller/MainController.java
+++ src/main/java/project/diabetes/controller/MainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/main")
+    @GetMapping("/record")
     public String main(){
-        return "main";
+        return "record";
     }
 
     @GetMapping("/first")
